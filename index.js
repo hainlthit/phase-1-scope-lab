17ffd40037847f651ea8ed0831d9fad271135304
Hain Thit <hainlthit@gmail.com>
2021-12-14 12:25:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer 
+const leastFavoriteCustomer = 'susan'
+
+    function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName  
+    }
+
+    function setBestCustomer() {
+        bestCustomer = 'not bob'
+    }
+
+    function overwriteBestCustomer() {
+        bestCustomer = 'maybe bob'
+    }
+
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = 'karen'
+        return leastFavoriteCustomer
+    }
+
+
+    
+  
+
+
+    
+    
+
+
